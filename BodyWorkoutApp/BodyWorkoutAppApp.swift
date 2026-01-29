3ab10aa49578c28f06ed84721927813af5cf067b
Haseeb Ahmed <Haseeb@kahunas.io>
2026-01-29 15:09:17 +0400

    Initial Commit

---
+++ BodyWorkoutApp/BodyWorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyWorkoutAppApp.swift
+//  BodyWorkoutApp
+//
+//  Created by Haseeb Ahmed on 29/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BodyWorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BodyWorkoutAppTests/BodyWorkoutAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BodyWorkoutAppTests.swift
+//  BodyWorkoutAppTests
+//
+//  Created by Haseeb Ahmed on 29/01/2026.
+//
+
+import Testing
+@testable import BodyWorkoutApp
+
+struct BodyWorkoutAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
